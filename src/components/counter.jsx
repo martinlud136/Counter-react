6a4f8f270978bcc363633a489d6b686fd6efa324
Martinlud <hectormartinluduena@gmail.com>
2020-05-20 19:52:52 -0300

    componente

---
+++ src/components/counter.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+    state = {
+        count: 0
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Hello World</h1>
+                <button>increment</button>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Counter;
