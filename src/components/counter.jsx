2e92f7d436e5e63d99181ee6a7614f446f0bfba8
Martinlud <hectormartinluduena@gmail.com>
2020-05-21 17:25:51 -0300

    pasar valores de counter en un solo objeto

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        value: this.props.value,
+        value: this.props.counter.value,
     };
 
     formatCount() {
@@ -15,14 +15,18 @@ class Counter extends Component {
     };
 
     render() {
-
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">
                     increment
                 </button>
-                <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
+                <button
+                    onClick={() => this.props.onDelete(this.props.counter.id)}
+                    className="btn btn-danger btn-sm m-2"
+                >
+                    Delete
+                </button>
             </div>
         );
     }
